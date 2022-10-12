585d33b04b48db74ebc286e25859f02ca494ea8c
TakSuzu <takahisa@tsuda.ac.jp>
2022-10-12 19:43:08 +0900

    second ver.

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
+import pandas as pd
+import time
+import altair as alt
 
 st.title("hello")
 
-st.write("write")
-st.markdown("# Head1")
-st.markdown("## Head2")
+st.write("おためしです")
 
 
-check = st.checkbox("チェックボックス") #引数に入れることでboolを返す
+check = st.checkbox("チェックすると表示される") #引数に入れることでboolを返す
 
 if check:
    st.button("ボタン") #引数に入れるとboolで返す
@@ -19,7 +20,58 @@ if check:
 
 
 # 以下をサイドバーに表示
-st.sidebar.text_input("文字入力欄２") #引数に入力内容を渡せる
-st.sidebar.text_area("テキストエリア２")
+st.sidebar.text_input("サイドバー文字入力欄") #引数に入力内容を渡せる
+#st.sidebar.text_area("テキストエリア２")
 
 
+@st.cache
+def get_UN_data():
+    df = pd.read_csv("Covid_infection.csv")
+    return df.set_index("date")
+
+
+df = get_UN_data()
+
+
+data = df.loc[:,"num"]
+
+df.index = pd.to_datetime(df.index)
+
+st.markdown("# 感染者数の推移グラフ")
+
+data = data.T.reset_index()
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = data.loc[0:1,"num"]
+#chart = st.line_chart(last_rows)
+chart = st.line_chart(df.loc[:,"num"][0:10])
+
+for i in range(1, len(df)):
+    #status_text.text("%i%% Complete" % i)
+    #chart.add_rows(data.loc[i:i+1,"num"])
+    chart.add_rows(df.loc[:,"num"][i:i+1])
+    #progress_bar.progress(i)
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
+
+
+st.markdown("## パターン2")
+
+
+chart = (
+    alt.Chart(data)
+    .mark_circle()#area(opacity=0.7)
+    .encode(
+        x="date:T",
+        y=alt.Y("感染者数:Q", stack=None),
+        color="Region:N",
+    )
+)
+st.altair_chart(chart, use_container_width=True)
